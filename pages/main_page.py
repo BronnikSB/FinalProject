18f652149953cbb727113cbb5a790517d67f5537
Vladislav <bronks.work@gmail.com>
2022-06-23 22:45:24 +0300

    Реализованы новые методы: go_to_login_page should_be_login_link

---
+++ pages/main_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.element(*MainPageLocators.LOGIN_LINK)
